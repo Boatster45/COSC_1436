b0dddb2c64e10d505b2c825d2e1add919af070c5
Boatster45 <johnathankissee@gmail.com>
2024-09-09 20:43:24 -0500

    2nd prog assignment

---
+++ Programming_2/MadLib.cpp
@@ -0,0 +1,56 @@
+//
+//  MadLib.cpp
+//  
+//
+//  Created by Bruce Gooch on 9/13/21.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //Fill This Field With A Random Word Fill All Randomizable Fields
+    
+
+    string VERB = "paint";
+    string ADJECTIVE = "huge";
+    string PLURAL_NOUN = "bananas";
+    string ADJECTIVE2 = "";
+    string VERB_ENDING_IN_ING = "";
+    string VERB2  = "";
+    string NUMBER  = "";
+    string ADJECTIVE3  = "";
+    string PLURAL_NOUN2  = "";
+    string PLURAL_NOUN3  = "";
+    string PLURAL_NOUN4  = "";
+    string PLURAL_RELATIVE  = "";
+    string ADJECTIVE4  = "";
+    string ADJECTIVE5  = "";
+    string PLURAL_NOUN5  = "";
+
+    cout << endl; // Print blank line
+    cout << endl;
+    cout << endl;
+    
+    cout << "Come "<< VERB << " at WALMART, where you`ll receive " << ADJECTIVE << " discounts on all of your favorite brand name " << PLURAL_NOUN << "." << endl;
+ 
+    
+/*
+ 
+
+ Our ADJECTIVE2 and VERB_ENDING_IN_ING associates are there to VERB2 you NUMBER hours a day.
+ 
+ Here you will find ADJECTIVE3 prices on the PLURAL_NOUN2 you need. PLURAL_NOUN3 for the moms, PLURAL_NOUN4 for the kids and all the latest electronics for the RELATIVE.
+ 
+ So come on down to your ADJECTIVE4 ADJECTIVE5 WALMART where the PLURAL_NOUN5 come first.
+ 
+*/
+    
+    
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    
+    return 0;
+}
